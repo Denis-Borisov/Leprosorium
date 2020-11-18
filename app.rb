8c738036c270561f40ec70ad31a7df2a052e2237
Denis <buh.borisov@yandex.ru>
2020-11-18 22:54:52 +0300

    Добавление проверки параметров

--- app.rb
+++ app.rb
@@ -10,6 +10,8 @@ def init_db
  	@db.results_as_hash = true	
 end
 
+# вызывается при перезагрузке любой страницы
+
 before do
 	# инициализация БД
 
@@ -32,15 +34,24 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
-
+# обработчик get-запроса / new
+# (браузер получает страницу с сервера)
 
 get '/new' do
   erb :new
 end
 
+# обработчик post-запроса / new
+# (браузер отправляет данные на сервер)
+
 post '/new' do
   # получаем переменную из post-запроса
   content = params[:content]
+
+  if content.length <= 0
+  	@error = 'Type post text'
+  	return erb :new
+  end
 
   erb "You typed #{content}"
 
