1870b0364271eab394c601b500cb7a4feb337a02
Denis <buh.borisov@yandex.ru>
2020-11-19 21:54:31 +0300

    Исправление переменной results

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ end
 get '/' do
 	# выбираем список постов из БД
 
-	results = @db.execute 'select * from Posts order by id desc'
+	@results = @db.execute 'select * from Posts order by id desc'
 
 	erb :index			
 end
