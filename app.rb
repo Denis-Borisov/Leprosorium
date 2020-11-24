37ca2fc21388dfe984baffa18040bd505b52ac68
Denis <buh.borisov@yandex.ru>
2020-11-24 21:37:05 +0300

    Добавление обработчика get '/details/:post_id' (для получение параметра из url)

--- app.rb
+++ app.rb
@@ -63,8 +63,17 @@ post '/new' do
 
   # перенаправление на главную страницу
 
-  redirect to '/' 
+  redirect to '/'   
 
-  
+end
+
+
+# вывод информации о посте
+
+get '/details/:post_id' do
+  post_id = params[:post_id]
+
+  erb "Displaying information for post with id #{post_id}"
+
 
 end
